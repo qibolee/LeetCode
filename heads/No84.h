dd63717dee8bed61200f6b8ae4d611cac2b66e45
qibolee <qibolee@163.com>
2016-10-11 08:59:18 +0800

    add solution for leetcode no84 problem

--- heads/No84.h
+++ heads/No84.h
@@ -12,13 +12,14 @@ public:
     int largestRectangleArea(std::vector<int> &heights) {
         heights.push_back(0);
         std::stack<int> mt;
-        int max = 0, tmp = 0;
+        int max = 0;
         for (int i = 0; i < heights.size(); ++i) {
             while (!mt.empty() && heights[mt.top()] > heights[i]) {
                 int ind = mt.top();
                 mt.pop();
+
                 int width = (mt.empty() ? i : i - mt.top() - 1);
-                tmp = heights[ind] * width;
+                int tmp = heights[ind] * width;
                 if (tmp > max) {
                     max = tmp;
                 }
