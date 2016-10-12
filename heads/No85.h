681afe268dd8424876ef90bb357d16fb618ce583
qibolee <qibolee@163.com>
2016-10-12 08:48:41 +0800

    solution for no85 problem, but timeout;

--- heads/No85.h
+++ heads/No85.h
@@ -15,20 +15,21 @@ public:
             return 0;
         }
         std::vector<std::vector<int>> nums(matrix.size(), std::vector<int>(matrix.front().size(), 0));
-        if (matrix.front().front() == 1) {
+        if (matrix.front().front() == '1') {
             nums.front().front() = 1;
         }
         for (int i = 1; i < matrix.front().size(); ++i) {//first row
-            nums[0][i] = nums[0][i - 1] + (matrix[0][i] == 1);
+            nums[0][i] = nums[0][i - 1] + (matrix[0][i] == '1');
         }
         for (int i = 1; i < matrix.size(); ++i) {//first col
-            nums[i][0] = nums[i - 1][0] + (matrix[i][0] == 1);
+            nums[i][0] = nums[i - 1][0] + (matrix[i][0] == '1');
         }
         for (int i = 1; i < matrix.size(); ++i) {//remain area
-            for (int j = 1; j < matrix.size(); ++j) {
-                nums[i][j] = nums[i - 1][j] + nums[i][j - 1] - nums[i - 1][j - 1] + (matrix[i][j] == 1);
+            for (int j = 1; j < matrix.front().size(); ++j) {
+                nums[i][j] = nums[i - 1][j] + nums[i][j - 1] - nums[i - 1][j - 1] + (matrix[i][j] == '1');
             }
         }
+
         std::vector<std::pair<int, int>> seq;
         for (int h = nums.size(); h > 0; h--) {
             for (int w = nums.front().size(); w > 0; w--) {
@@ -39,7 +40,7 @@ public:
                   [](const std::pair<int, int> &par1, const std::pair<int, int> &par2) {
                       return par1.first * par1.second > par2.first * par2.second;
                   });
-        int max = 0;
+        int max = (nums.back().back() > 0);
         bool need_to_exit = false;
 
         for (const std::pair<int, int> &par: seq) {
