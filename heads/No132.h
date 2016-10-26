c754c080e825ee45e72ae806591b5a728f92dd4e
qibolee <qibolee@163.com>
2016-10-26 15:38:32 +0800

    optimise the no132 solution

--- heads/No132.h
+++ heads/No132.h
@@ -8,26 +8,25 @@
 #include "head.h"
 
 class Solution {
-private:
-    bool isPal(const std::string &s, int beg, int len) {
-        for (int i = 0; i < len / 2; ++i) {
-            if (s[beg + i] != s[beg + len - 1 - i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     int minCut(std::string s) {
         if (s.size() < 2) {
             return 0;
         }
+        std::vector<std::vector<bool>> pal(s.size(), std::vector<bool>(s.size(), false));
         std::vector<int> lastTwoCut;
         std::vector<int> dp(s.size(), 0);
         lastTwoCut.push_back(0);
+        pal.front().front() = true;
+        for (int j = 1; j < pal.size(); ++j) {
+            for (int i = j; i >= 0; --i) {
+                if (s[i] == s[j] && (j - i < 2 || pal[i + 1][j - 1])) {
+                    pal[i][j] = true;
+                }
+            }
+        }
         for (int i = 1; i < dp.size(); ++i) {
-            if ((dp[i - 1] < 2) && isPal(s, 0, i + 1)) {
+            if ((dp[i - 1] < 2) && pal[0][i]) {
                 dp[i] = 0;
                 lastTwoCut.clear();
                 lastTwoCut.push_back(i);
@@ -39,7 +38,7 @@ public:
             } else {
                 int tmp = lastTwoCut.front();
                 for (int j = std::max(tmp + tmp - i + 1, 1); j < i; ++j) {
-                    if ((dp[j - 1] < dp[i - 1]) && isPal(s, j, i - j + 1)) {
+                    if ((dp[j - 1] < dp[i - 1]) && pal[j][i]) {
                         dp[i] = dp[j - 1] + 1;
                         lastTwoCut.front() = j - 1;
                         lastTwoCut.back() = i;
@@ -49,7 +48,7 @@ public:
 
                 if (dp[i] == 0) {
                     for (int j = std::max(tmp + tmp - i + 1, 1); j < i; ++j) {
-                        if ((dp[j - 1] == dp[i - 1]) && isPal(s, j, i - j + 1)) {
+                        if ((dp[j - 1] == dp[i - 1]) && pal[j][i]) {
                             dp[i] = dp[j - 1] + 1;
                             lastTwoCut.front() = j - 1;
                             lastTwoCut.back() = i;
