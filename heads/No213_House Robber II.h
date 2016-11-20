7fc54340c995a385000e60b71878c421829d5c2f
qibolee <qibolee@163.com>
2016-11-20 11:31:29 +0800

    add solution for no213

--- heads/No213_House Robber II.h
+++ heads/No213_House Robber II.h
@@ -8,6 +8,19 @@
 #include "head.h"
 
 class Solution {
+private:
+    int rob2(const std::vector<int> &nums) {
+        int a = nums[0];
+        int b = std::max(nums[0], nums[1]);
+        int c = 0;
+        for (int i = 2; i < nums.size(); ++i) {
+            c = std::max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
 public:
     int rob(std::vector<int> &nums) {
         if (nums.empty()) {
@@ -16,52 +29,11 @@ public:
         if (nums.size() == 1) {
             return nums.front();
         }
-        int first1 = 0;
-        int last1 = 0;
-        int first2 = 0;
-        int last2 = 0;
-        int first3 = 0;
-        int last3 = 0;
-
-        std::vector<int> dp(nums.size(), 0);
-        dp.front() = nums.front();
-        if (nums[1] < nums[0]) {
-            first2 = 0;
-            last2 = 0;
-            dp[1] = nums[0];
-        } else {
-            first2 = 1;
-            last2 = 1;
-            dp[1] = nums[1];
+        if (nums.size() == 2) {
+            return std::max(nums[0], nums[1]);
         }
-        for (int i = 2; i < dp.size(); ++i) {
-
-            if (dp[i - 1] > dp[i - 2] + nums[i]) {
-                dp[i] = dp[i - 1];
-                first3 = first2;
-                last3 = last2;
-            } else if (dp[i - 1] < dp[i - 2] + nums[i]) {
-                dp[i] = dp[i - 2] + nums[i];
-                first3 = first1;
-                last3 = i;
-            } else {
-                if (first1 < first2) {
-                    dp[i] = dp[i - 1];
-                    first3 = first2;
-                    last3 = last2;
-                } else {
-                    dp[i] = dp[i - 2] + nums[i];
-                    first3 = first1;
-                    last3 = i;
-                }
-            }
-            first1 = first2;
-            last1 = last2;
-            first2 = first3;
-            last2 = last3;
-        }
-        if()
-
+        return std::max(rob2(std::vector<int>(nums.begin(), nums.end() - 1)),
+                        rob2(std::vector<int>(nums.begin() + 1, nums.end())));
     }
 };
 
